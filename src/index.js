53a22bfe241dfe7aa1187efe369ef21f3a880779
Richard Zhang <geteralzx@gmail.com>
2020-04-07 14:17:21 -0500

    set up cart

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+
+import Cart from './components/Cart';
+import AddProduct from './components/AddProduct';
+import { createStore } from 'redux';
+import BaseLayout from './components/layout/BaseLayout'
+
+
+// import App from './App';
+//Create counter component
+//////////////// Redux/////////////////////////
+//dispatch
+//mapsStateToProps
+//Action Creator
+//Reducer(state,action)
+//create store (reducer)
+
+// let store = createStore(counterReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+//connect component to connect mapStateToProps
+//wrap out application inside Provider
+//pass to the proviver to store
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <Provider>
+    <BrowserRouter>
+      <BaseLayout>
+        <Switch>
+          <Route exact path="/" component={Cart} />
+          <Route exact path="/addProduct" component={AddProduct} />
+        </Switch>
+      </BaseLayout>
+    </BrowserRouter>
+  // </Provider>
+
+
+  ,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
